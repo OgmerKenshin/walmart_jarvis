de6030f68996a5f10c886ea3646be1a050df14b0
OgmerKenshin <kenshinaogmer@gmail.com>
2025-08-29 02:20:19 +0800

    initialized api in separate env file

---
+++ jarvis.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AGENT_ID = os.getenv("agent_0001k3rwnd1vej5swt868gsp4hj1")
+API_KEY = os.getenv("sk_e535a7f7cd7b0467e9c1924b884895474f2ddea0a8a867f7")
